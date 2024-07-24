56c26eaa70946bf3b544e66e87d308be924ed62f
Manuel López Mallorquín <manuel.lopez@madisonmk.com>
2024-07-24 19:05:13 +0200

    Save active rules, accent color and dark mode into shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:therules/src/providers/shared_preferences_provider.dart';
 import 'package:therules/src/providers/theme_provider.dart';
 import 'package:therules/src/screens/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
   runApp(
-    const ProviderScope(
-      child: TheRules(),
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: const TheRules(),
     ),
   );
 }
--- lib/src/models/rule.dart
+++ lib/src/models/rule.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Rule {
   Rule({
+    required this.id,
     required this.imagePath,
     required this.name,
     required this.description,
@@ -10,6 +11,7 @@ class Rule {
     this.onFinish,
   });
 
+  String id;
   String imagePath;
   String name;
   String description;
